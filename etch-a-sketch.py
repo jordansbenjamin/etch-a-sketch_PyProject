cfffd4a8fbc0139fd5feb0cf5b0aa903945e937f
jordansbenjamin <14327@coderacademy.edu.au>
2023-06-05 18:25:22 +1000

    Added & tested forward event listener

--- etch-a-sketch.py
+++ etch-a-sketch.py
@@ -5,4 +5,20 @@ screen = Screen()
 
 pen.shape('classic')
 
+# Forwards
+def forwards():
+    pen.forward(10)
+
+# Backwards
+
+# Rotate counter-clockwise (left)
+
+# Rotate clockwise (right)
+
+# Clear drawing
+
+screen.listen()
+
+screen.onkey(key='w', fun=forwards)
+
 screen.exitonclick()
